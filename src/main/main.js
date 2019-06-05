2dcae1aaa5c4ae9c69ace13a55453c4fe07d6469
Donald Blanc <dxb3662@rit.edu>
2019-06-05 19:40:31 -0400

    restructured the folders and cleaned up main to follow better best practices

---
+++ src/main/main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadFile('index.html');
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+
+  if (mainWindow === null) createWindow();
+});
