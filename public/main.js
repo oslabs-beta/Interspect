d4aea86c25d0c2747da823819d581ada77a96d3f
Donald Blanc <dxb3662@rit.edu>
2019-06-06 16:44:37 -0400

    added the ability to close the app when the x is hit. remove the window-all closed listner until we figure out approprate behavior

--- public/main.js
+++ public/main.js
@@ -24,13 +24,4 @@ function createWindow() {
     mainWindow = null;
   });
 }
-
 app.on('ready', createWindow);
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
-});
-
-app.on('activate', () => {
-  if (mainWindow === null) createWindow();
-});
