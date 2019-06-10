f2755af08ed6db0ffd52973b3ba07321c867f347
Conor Sexton <sextonc@me.com>
2019-06-10 16:35:02 -0400

    Incorporate request bar to source panel and rename ViewTest for clarity (now DataTree)
    Co-authored-by: Joel Rivera realjoelrivera@gmail.com
    Co-authored-by: Conor Sexton sextonc@me.com

--- src/main/containers/SourcePanel.jsx
+++ src/main/containers/SourcePanel.jsx
@@ -4,13 +4,13 @@ import RequestBar from '../components/RequestBar.jsx';
 
 const SourcePanel = () => {
   const [isLoading, setLoading] = useState(false);
-  const [data, setData] = useState({});
+  const [data, setData] = useState(undefined);
 
   return (
     <section className='panel'>
       <h1>Data source panel</h1>
       <RequestBar SourceOrDest='source' requestBody='' />
-      <DataCanvas data={}/>
+      <DataCanvas data={data}/>
     </section>
   );
 };
