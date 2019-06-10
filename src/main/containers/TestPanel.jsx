9f383487e183a7ccd62829ec30a7b9becbdf56f5
Donald Blanc <dxb3662@rit.edu>
2019-06-10 09:43:45 -0400

    added mock data to test the rendering

--- src/main/containers/TestPanel.jsx
+++ src/main/containers/TestPanel.jsx
@@ -1,11 +1,25 @@
 import React, { useState } from 'react';
+import ViewTest from '../../renderer/testWindow/formatedView/ViewTest.jsx';
+
+// will need to get data from the get request to pass to the formated view
 
 const TestPanel = (props) => {
   const [mockups, setData] = useState([props.data]);
+  // sample JSON to pass down as props. Will be able to remove as the project evolves.
+  const data = {
+    firstName: 'Hello2',
+    lastName: 'Joe',
+    phones: [
+      '123-45-67',
+      '987-65-43',
+    ],
+  };
+
 
   return (
-    <section className='panel'>
+    <section className='panel'style= {{ maxHeight: '400px', overflow: 'auto' }} >
       <p>Test panel</p>
+      <ViewTest testdata={data}/>
     </section>
   );
 };
