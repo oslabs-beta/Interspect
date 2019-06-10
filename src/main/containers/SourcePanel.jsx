2831f432dc60e622da948565da780427786dd675
Conor Sexton <sextonc@me.com>
2019-06-10 19:18:28 -0400

    Render data from HTTP GET request
    Co-authored-by: Donald Blanc dxb3662@rit.edu
    Co-authored-by: Conor Sexton sextonc@me.com

--- src/main/containers/SourcePanel.jsx
+++ src/main/containers/SourcePanel.jsx
@@ -5,14 +5,14 @@ import { smallData } from '../dummyData';
 
 const SourcePanel = (props) => {
   const [isLoading, setLoading] = useState(false);
-  const [data, setData] = useState(smallData);
+  const [data, setData] = useState(undefined);
 
   const { treeCount, updateTreeCount } = props;
 
   return (
     <section className='panel'>
       <h1>Data source panel</h1>
-      <RequestBar SourceOrDest='source' requestBody='' />
+      <RequestBar SourceOrDest='source' setData={setData}/>
       <DataCanvas treeCount={treeCount} updateTreeCount={updateTreeCount} data={data}/>
     </section>
   );
