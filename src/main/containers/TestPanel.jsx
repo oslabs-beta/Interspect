15584246b271048ba04733a23fd5f6dcd6d42194
Donald Blanc <dxb3662@rit.edu>
2019-06-10 15:09:33 -0400

    passed the sample json from file and passing it down as props

--- src/main/containers/TestPanel.jsx
+++ src/main/containers/TestPanel.jsx
@@ -1,24 +1,17 @@
 import React, { useState } from 'react';
 import ViewTest from '../components/ViewTest.jsx';
+// sample JSON to pass down as props. Will be able to remove as the project evolves.
+import { largeData, smallData } from '../dummyData';
 
 // will need to get data from the get request to pass to the formatted view
 
 const TestPanel = (props) => {
   const [mockups, setData] = useState([props.data]);
-  // sample JSON to pass down as props. Will be able to remove as the project evolves.
-  const data = {
-    firstName: 'Hello2',
-    lastName: 'Joe',
-    phones: [
-      '123-45-67',
-      '987-65-43',
-    ],
-  };
 
   return (
     <section className='panel' >
       <p>Test panel</p>
-      <ViewTest testdata={data} style= {{ maxHeight: '400px', overflow: 'auto' }}/>
+      <ViewTest testdata={largeData}/>
     </section>
   );
 };
