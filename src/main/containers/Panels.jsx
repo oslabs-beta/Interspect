4d0240aa748a3292bac2721488fd8d423351326f
Donald Blanc <dxb3662@rit.edu>
2019-06-11 20:25:02 -0400

    ran linting rules

--- src/main/containers/Panels.jsx
+++ src/main/containers/Panels.jsx
@@ -7,18 +7,26 @@ const Panels = () => {
   const [activePanel, setActivePanel] = useState('source');
   const [dataTreeCount, setDataTreeCount] = useState(0);
   const [data, setData] = useState(undefined);
-  // Tests are objects with 
+  // Tests are objects with
   // { payload: JSON that represents test,
   //   status: initial value of '' };
   const [tests, setTests] = useState(
-    [ {payload : JSON.stringify({ message: 'hello by joe', created_by: 'joe' }),
-        status: '' }, 
-      {payload : JSON.stringify({ message: 'hello by conor', created_by: 'conor' }),
-        status: '' },
-      {payload : JSON.stringify({ message: 'hello by joe2', created_by: 'joe' }),
-        status: '' }, 
-      {payload : JSON.stringify({ message: 'hello by conor2', created_by: 'conor' }),
-        status: '' } ]
+    [{
+      payload: JSON.stringify({ message: 'hello by joe', created_by: 'joe' }),
+      status: '',
+    },
+    {
+      payload: JSON.stringify({ message: 'hello by conor', created_by: 'conor' }),
+      status: '',
+    },
+    {
+      payload: JSON.stringify({ message: 'hello by joe2', created_by: 'joe' }),
+      status: '',
+    },
+    {
+      payload: JSON.stringify({ message: 'hello by conor2', created_by: 'conor' }),
+      status: '',
+    }],
   );
 
   return (
