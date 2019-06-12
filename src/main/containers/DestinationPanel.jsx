d91fb5c5f4da64487d01bfe42e22103c066789a5
Donald Blanc <dxb3662@rit.edu>
2019-06-11 20:27:45 -0400

    ran linting rules

--- src/main/containers/DestinationPanel.jsx
+++ src/main/containers/DestinationPanel.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import RequestBar from './../components/RequestBar.jsx';
+import RequestBar from '../components/RequestBar.jsx';
 import ResponseComponent from '../components/ResponseComponent.jsx';
 
 const DestinationPanel = (props) => {
@@ -7,7 +7,7 @@ const DestinationPanel = (props) => {
 
   // will be props.testBodies but not connected yet :D
   const responseComponentsList = [];
-  for (let i = 0; i < tests.length; i++) {
+  for (let i = 0; i < tests.length; i += 1) {
     responseComponentsList.push(
       <ResponseComponent
         status={tests[i].status}
@@ -15,9 +15,9 @@ const DestinationPanel = (props) => {
 
         // fix later
         key={tests[i].payload}
-      />
+      />,
     );
-  };
+  }
 
   return (
     <section className='panel'>
