6bf815e38b637f5a83d68ba55ddc1bb58c3b5bda
Conor Sexton <sextonc@me.com>
2019-06-13 11:17:57 -0400

    Adds left border to inline inputs to delineate from other form elements

--- src/main/components/InlineInput.jsx
+++ src/main/components/InlineInput.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 const InlineInput = styled.input`
   border: none;
+  border-left: 1px solid #D8D8D8;
   color: #292F32;
   font-family: 'Halyard Text', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   font-size: 1em;
