e8a7bf8b415ffd9e73a1caac258c99af2d7ad219
Donald Blanc <dxb3662@rit.edu>
2019-06-12 20:01:32 -0400

    change the state of the header and moved it up to the request bar

--- src/main/components/HeaderBar.jsx
+++ src/main/components/HeaderBar.jsx
@@ -1,33 +1,23 @@
 import React, { useState, useEffect } from 'react';
 
 const HeaderBar = (props) => {
-  console.log(props);
-  const headKey = 'Authorization';
-  const AuthType = 'Bearer Token';
-  const [selected, setSelected] = useState(headKey);
-  const [type, setType] = useState(AuthType);
-  const [headerKey, setHeaderKey] = useState('');
-  console.log(selected);
+  const {
+    header, authType, handleChange,
+  } = props;
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    console.log(name);
-    if (name === 'header') setSelected(value);
-
-  };
 
   return (
     <div>
       <p>Header Bar</p>
       <form>
-      <select name='header' id='headerTypeInput' multiple={false} value={selected}
+      <select name='header' id='headerTypeInput' multiple={false} value={header}
             onChange={handleChange} >
             <option value='Authorization'>Authorization</option>
             <option value='NONE'>none</option>
           </select>
-          <select name='authType' id='typeInput' multiple={false} value={type}
+          <select name='authType' id='typeInput' multiple={false} value={authType}
             onChange={handleChange} >
-            <option value={type}>{type}</option>
+            <option value={authType}>{authType}</option>
           </select>
           <input name='headerKey' id='headerInput' type='text' onChange={handleChange}></input>
       </form>
