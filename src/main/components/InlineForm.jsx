1857692a7f061ecc4407aa25ad118aa68858a047
Conor Sexton <sextonc@me.com>
2019-06-13 11:25:22 -0400

    Adds simple “active state” to focused inline forms

--- src/main/components/InlineForm.jsx
+++ src/main/components/InlineForm.jsx
@@ -5,6 +5,11 @@ const InlineForm = styled.form`
   border: 1px solid #BCC1C2;
   border-radius: 3px;
   display: flex;
+  transition: all 0.15s ease-in-out
+  &:focus-within {
+    border: 1px solid #949C9E;
+    box-shadow: 0 2px 6px -2px rgba(41, 47, 50, 0.25);
+  }
 `;
 
 export default InlineForm;
