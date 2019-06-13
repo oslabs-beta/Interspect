ff8af615227cdfd37347fbc4ad902a926a40c1c7
Conor Sexton <sextonc@me.com>
2019-06-13 18:22:26 -0400

    Makes top data panel sticky

--- src/main/containers/StyledPanel.jsx
+++ src/main/containers/StyledPanel.jsx
@@ -6,8 +6,10 @@ const StyledPanel = styled.section`
     if (props.active) return 'none;';
     return '1px solid #F0F3F4;';
   }}
-  padding: 1em 2em;
-  width: 33.3333vw;
+  max-height: 80vh;
+  overflow-y: scroll;
+  padding: 0 2em 1em;
+  position: relative;
   width: ${props => (props.active ? '80vw' : '10vw')};
   h1, h3 {
     color: #BCC1C2;
@@ -19,6 +21,16 @@ const StyledPanel = styled.section`
   }
   h3 {
     text-align: left;
+  }
+  &>div {
+    background-color: #FFFFFF;
+    border-bottom: 1px solid #F0F3F4;
+    margin-bottom: 2em;
+    margin-top: 1em;
+    padding-bottom: 2em;
+    position: sticky;
+    top: 0px;
+    z-index: 1;
   }
 `;
 
