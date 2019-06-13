2f9c8f77c4bf4387aef0447cc70724ceb834b3b4
Conor Sexton <sextonc@me.com>
2019-06-13 14:43:33 -0400

    Fixed issue where button overflowed from inline form

--- src/main/components/InlineForm.jsx
+++ src/main/components/InlineForm.jsx
@@ -5,10 +5,21 @@ const InlineForm = styled.form`
   border: 1px solid #BCC1C2;
   border-radius: 3px;
   display: flex;
+  flex-shrink: 1;
+  justify-content: flex-start;
   transition: all 0.15s ease-in-out
   &:focus-within {
     border: 1px solid #949C9E;
     box-shadow: 0 2px 6px -2px rgba(41, 47, 50, 0.25);
+  }
+  button {
+    border-radius: 2px;
+    margin-left: auto;
+    margin-right: 0.5em;
+    // min-width: 0;
+  }
+  input {
+    min-width: 0;
   }
 `;
 
