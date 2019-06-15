d5c40a4b2924ccc3f71329a8d954da3b13571947
Conor Sexton <sextonc@me.com>
2019-06-15 16:10:51 -0400

    Removed heading from main app window

--- src/main/containers/App.jsx
+++ src/main/containers/App.jsx
@@ -1,10 +1,10 @@
 import { hot } from 'react-hot-loader/root';
+import styled from 'styled-components'
 import React from 'react';
 import Panels from './Panels.jsx';
 
 const App = () => (
    <div>
-      <h1>An Open-Source API Data Mocking Tool</h1>
       <Panels/>
    </div>
 );
