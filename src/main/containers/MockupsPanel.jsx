0b80ea58653289f611de7e443cd2ca8cbf7b7a13
Conor Sexton <sextonc@me.com>
2019-06-18 12:04:57 -0400

    Adds basic mockup naming functionality

--- src/main/containers/MockupsPanel.jsx
+++ src/main/containers/MockupsPanel.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useContext } from 'react';
-// sample JSON to pass down as props. Will be able to remove as the project evolves.
-// import { largeData, smallData } from '../dummyData';
+import styled from 'styled-components';
 import StyledPanel from './StyledPanel.jsx';
 import { TestsContext } from '../testsContext';
 import Button from '../components/Button.jsx';
 import DataTree from '../components/DataTree.jsx';
+import Form from '../components/InlineForm.jsx';
+import Input from '../components/InlineInput.jsx';
 
 // will need to get data from the get request to pass to the formatted view
 const MockupsPanel = (props) => {
@@ -38,20 +39,52 @@ const MockupsPanel = (props) => {
     setTestsDiff(testsDiffClone);
   }
 
-  const testsDisplayList = [];
+  const Name = styled.h3`
+    font-family: 'Halyard Text', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    color:  #999;
+  `;
+
+  const Label = styled.label`
+    color:  #999;
+    font-family: 'Halyard Text', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 0.9em;
+    text-transform: uppercase;
+  `;
+
+  const ModForm = styled(Form)`
+    flex-direction: column;
+    align-items: flex-start;
+  `;
+
+  const updateName = (name, index) => {
+    const testsCopy = [...tests];
+    testsCopy[index].name = name;
+    setTests(testsCopy);
+  };
+
+  const mockupsListDisplay = [];
   let i = 0;
-  tests.forEach((test) => {
-    testsDisplayList.push(
-      <div>
-        <h3 id={'title_' + i}>NAME</h3>
+  tests.forEach((test, index) => {
+    const { name } = test;
+    
+    const NameForm = <ModForm onSubmit={(e) => {
+        e.preventDefault();
+        updateName(e.target.children[0].value, index);
+      }}>
+      <Input name="name" placeholder={`Test #${i + 1}`} onBlur={e => updateName(e.target.value, index)}></Input>
+      <Label htmlFor="name">Name</Label>
+    </ModForm>;
+    mockupsListDisplay.push(
+      <article className='mockup' key={`mockup-${i}`}>
+        {name ? <Name onClick={() => updateName('', index)}>{name}</Name> : NameForm }
         <DataTree
           treeId={i}
-          key={`TestPanelDataTree ${i}`}
           data={test.payload}
+          name={''}
           options={dataTreeOptions}
           saveUpdatedTree={saveUpdatedTree}
         />
-      </div>,
+      </article>,
     );
     i += 1;
   });
@@ -59,7 +92,7 @@ const MockupsPanel = (props) => {
   function createNewTest() {
     const testsClone = [...tests];
     const testsDiffClone = [...testsDiff];
-    testsClone.push({ payload: data, status: '' });
+    testsClone.push({ payload: data, status: '', name: '' });
     testsDiffClone.push({});
 
     // the ID of the test will be the same as the position in the array
@@ -74,9 +107,9 @@ const MockupsPanel = (props) => {
           <div>
             {data && <h3>Server Response</h3>}
             {datacanvas}
-            {data && <Button enabled={true} onClick={createNewTest}> New Test </Button>}
+            {data && <Button enabled={true} onClick={createNewTest}>New Test</Button>}
           </div>
-          {testsDisplayList}
+          {mockupsListDisplay}
         </StyledPanel>
     );
   }
