26823cb075d0e5ad99cf63a344423dca7db177e8
Conor Sexton <sextonc@me.com>
2019-06-21 11:53:41 -0400

    Modularizes mockup code into separate components

--- src/main/containers/MockupsPanel.jsx
+++ src/main/containers/MockupsPanel.jsx
@@ -4,8 +4,8 @@ import StyledPanel from './StyledPanel.jsx';
 import { TestsContext } from '../testsContext';
 import Button from '../components/Button.jsx';
 import DataTree from '../components/DataTree.jsx';
-import Form from '../components/InlineForm.jsx';
-import Input from '../components/InlineInput.jsx';
+// import NameForm from '../components/NameForm.jsx';
+import Mockup from '../components/Mockup.jsx';
 
 // will need to get data from the get request to pass to the formatted view
 const MockupsPanel = (props) => {
@@ -16,116 +16,14 @@ const MockupsPanel = (props) => {
   const [tests, setTests] = useContext(TestsContext);
   const [testsListCounter, setTestsListCounter] = useState(0);
 
-  const dataTreeOptions = {
-    onAdd: true,
-    onEdit: true,
-    onDelete: true,
-    enableClipboard: false,
-  };
-
-  function saveUpdatedTree(newData, arrayPosition, newValue, name, namespace) {
-    // Update tests
-    const testsClone = [...tests];
-    testsClone[arrayPosition].payload = newData;
-    setTests(testsClone);
-
-    // Update tests differences/changes
-    const testsDiffClone = [...testsDiff];
-    // for deletion-- find better syntax
-    if (!testsDiffClone[arrayPosition][namespace]) {
-      testsDiffClone[arrayPosition][namespace] = {};
-    }
-    testsDiffClone[arrayPosition][namespace][name] = newValue;
-    setTestsDiff(testsDiffClone);
-  }
-
-  const Name = styled.h3`
-    font-family: 'Halyard Text', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    color: #292F32;
-  `;
-
-  const Label = styled.label`
-    color:  #BCC1C2;
-    font-family: 'Halyard Text', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    font-size: 0.75em;
-  `;
-
-  const NameInput = styled(Input)`
-    padding: 0;
-    padding-bottom: 0.25em;
-  `;
-
-  const ModForm = styled(Form)`
-    flex-direction: column;
-    align-items: flex-start;
-    margin: 1em auto;
-  `;
-
-  const MockupHeader = styled.header`
-    display: flex;
-    align-items: flex-end;
-    h3 {
-      margin-bottom: 0;
-    }
-  `;
-
-  const EditButton = styled.button`
-    background: none;
-    border: none;
-    color: #4299EA;
-    font-family: 'Halyard Text', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    letter-spacing: 0.05em;
-    margin: 0 auto 0.25em 0.825em;
-    text-transform: uppercase;
-    opacity: ${props => (props.visible ? 1 : 0)};
-    transition: all 0.2s ease-in-out;
-    &:hover {
-      cursor: pointer;
-      /* opacity: 1; */
-    }
-    &:active, &:focus {
-      color: #1F4E7A;
-      outline: 0;
-    }
-  `;
-
-  const updateName = (name, index) => {
-    const testsCopy = [...tests];
-    testsCopy[index].name = name;
-    setTests(testsCopy);
-  };
-
-  const [visibility, setVisibility] = useState(false);
-  // let visibility = false;
-
   const mockupsListDisplay = [];
   let i = 0;
   tests.forEach((test, index) => {
     const { name } = test;
-    
-    const NameForm = <ModForm onSubmit={(e) => {
-        e.preventDefault();
-        updateName(e.target.children[0].value, index);
-      }}>
-      <NameInput name="name" placeholder={`Test #${i + 1}`} onBlur={e => updateName(e.target.value, index)}></NameInput>
-      <Label htmlFor="name">Name</Label>
-    </ModForm>;
+
     mockupsListDisplay.push(
-      <article className='mockup' key={`mockup-${i}`}>
-        {name
-          ? <MockupHeader onMouseEnter={() => { setVisibility(true); }} onMouseLeave={() => setVisibility(false)}>
-              <Name>{name}</Name>
-              <EditButton onClick={() => updateName('', index)} visible={visibility}>Edit</EditButton>
-            </MockupHeader>
-          : NameForm }
-        <DataTree
-          treeId={i}
-          data={test.payload}
-          name={''}
-          options={dataTreeOptions}
-          saveUpdatedTree={saveUpdatedTree}
-        />
-      </article>,
+      // Creates a component for each test to display name and data
+      <Mockup key={index} test={test} index={index}/>,
     );
     i += 1;
   });
