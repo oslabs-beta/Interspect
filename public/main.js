0394c71b0726c3cd22494b6db5055dae3656432d
Joe <20054730+joepav@users.noreply.github.com>
2019-06-21 14:02:22 -0400

    app now can listen for and console.log post reqs on post url

--- public/main.js
+++ public/main.js
@@ -2,6 +2,7 @@ const { app, BrowserWindow } = require('electron');
 const isDev = require('electron-is-dev');
 const path = require('path');
 const os = require('os');
+const express = require('express');
 
 if (isDev) {
   console.log('Running in development');
@@ -12,6 +13,7 @@ if (isDev) {
 let mainWindow;
 
 function createWindow() {
+  const expressApp = express();
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -23,7 +25,6 @@ function createWindow() {
 
   mainWindow.loadURL(isDev ? 'http://localhost:8080' : `file://${__dirname}/../dist/index.html`);
 
-
   if (isDev) {
     BrowserWindow.addDevToolsExtension(
       path.join(os.homedir(), './Library/Application Support/Google/Chrome/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/3.6.0_0'),
@@ -32,5 +33,13 @@ function createWindow() {
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
+
+  expressApp.post('/posturl', (request, response) => {
+    console.log(request);
+    response.status(200);
+    response.end();
+  });
+
+  expressApp.listen(3000);
 }
 app.on('ready', createWindow);
