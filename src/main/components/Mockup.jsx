eedb3385c2fb50f1b4caa41a65c663e603e5056a
Conor Sexton <sextonc@me.com>
2019-06-24 10:45:39 -0400

    Add support for additional mode—editing assertions

--- src/main/components/Mockup.jsx
+++ src/main/components/Mockup.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import NameForm from './NameForm.jsx';
 import MockupName from './MockupName.jsx';
 import DataTree from './DataTree.jsx';
+import Button from './Button.jsx';
+import AssertionsForm from './AssertionsForm.jsx'
 
 const dataTreeOptions = {
   onAdd: true,
@@ -21,9 +23,9 @@ const Mockup = (props) => {
 
   return (
     <article className="mockup" key={`mockup-${index}`}>
-      {mode === 'view'
-        ? <MockupName name={name || `Test #${index + 1}`} setMode={setMode} />
-        : <NameForm name={name} index={index} setMode={setMode} setName={setName} />
+      {mode === 'editName'
+        ? <NameForm name={name} index={index} setMode={setMode} setName={setName} />
+        : <MockupName name={name || `Test #${index + 1}`} setMode={setMode} />
       }
       <DataTree
           treeId={index}
@@ -32,6 +34,10 @@ const Mockup = (props) => {
           options={dataTreeOptions}
           saveUpdatedTree={saveUpdatedTree}
         />
+      {mode === 'editAssertions'
+        ? <AssertionsForm setMode={setMode} index={index}/>
+        : <Button enabled={true} onClick={() => setMode('editAssertions')}>Edit Test Assertions</Button>
+      }
     </article>
   );
 };
