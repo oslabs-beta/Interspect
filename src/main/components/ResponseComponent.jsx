eab57b6081e47c6947707e298bf5acb2ff38d469
Conor Sexton <sextonc@me.com>
2019-06-26 15:41:50 -0400

    Moves ResponseWrapper to its own component file

--- src/main/components/ResponseComponent.jsx
+++ src/main/components/ResponseComponent.jsx
@@ -1,41 +1,9 @@
 import React, { useState } from 'react';
 import ReactDiffViewer from 'react-diff-viewer';
 import styled from 'styled-components';
+import ResponseWrapper from './ResponseWrapper.jsx';
 
 // Styles
-const ResponseWrapper = styled.article`
-  background-color: #F0F3F4;
-  border-radius: 3px;
-  box-shadow: inset;
-  box-shadow: inset 0 0 2px 0 rgba(41, 47, 50, 0.25);
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  margin-bottom: 1em;
-  a {
-    color: #555B5E;
-    font-family: 'Halyard Text', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    font-size: 0.825em;
-    text-align: center;
-    :hover {
-      cursor: pointer;
-    }
-  }
-  div.diff {
-    border-top: 1px solid #E2E6E9;
-    margin: 0.25em;
-    max-height: 480px;
-    overflow-y: scroll;
-  }
-  table {
-    border-collapse: collapse;
-  }
-  pre {
-    font-family: 'IBM Plex Mono', monospace;
-    font-size: 90%;
-  }
-`;
-
 const CardButton = styled.button`
   background: #F0F3F4;
   border: none;
@@ -157,10 +125,6 @@ const ResponseComponent = (props) => {
       : `Expected status code to be ${expectedStatus[0]}`;
   };
 
-  const toggleDiff = () => {
-    setShowDiff(!showDiff);
-  };
-
   return (
     <ResponseWrapper>
       <Header>
@@ -173,10 +137,10 @@ const ResponseComponent = (props) => {
       { showDiff
         ? <div className="diff">
             <ReactDiffViewer
-            oldValue={JSON.stringify(data, null, 2)}
-            newValue={JSON.stringify(payload, null, 2)}
-            splitView={true}
-            styles={diffColors}
+              oldValue={JSON.stringify(data, null, 2)}
+              newValue={JSON.stringify(payload, null, 2)}
+              splitView={true}
+              styles={diffColors}
             />
           </div>
         : <CardButton onClick={() => setShowDiff(true)}>Show Diff</CardButton>
