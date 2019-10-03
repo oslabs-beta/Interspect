c5c7d30b0be02efa349f022d7ea33a4b71530deb
David Neuhuas <david@neuha.us>
2019-10-03 08:09:48 -0400

    Implments exporter action to update backend with bodyitems to mock.
    Front-end is now responsible for truncating customUrl down to just filepath

--- src/main/xserver/xroutes/xroutes.js
+++ src/main/xserver/xroutes/xroutes.js
@@ -1,10 +1,10 @@
-
 let router = require("express").Router();
 
 let urlFilePath = '';
+
 function setURLFilePath(url) {
-  urlFilePath = url
-  console.log(urlFilePath);
+    urlFilePath = url
+    console.log(urlFilePath);
 };
 
 // Function that dynamically sets routes based on passed in bodyItems object
@@ -25,50 +25,48 @@ function setURLFilePath(url) {
 // }
 
 const setDynamicRoutes = (req, res, next) => {
-  const methods = ['get', 'post', 'patch', 'put', 'delete'];
-  const bodyItems = req.body.bodyItems;
-  try {
-    for (let i = 0; i < methods.length; i++) {
-      for (let item in bodyItems) {
-        if (bodyItems[item].customMethod.toLowerCase() === methods[i]) {
-          router[methods[i]](bodyItems[item].customRoute, (req, res, next) => {
-            res.locals.response = bodyItems[item].customResponse;
-            next();
-          }, handleRequest);
+    const methods = ['get', 'post', 'patch', 'put', 'delete'];
+    const bodyItems = req.body.bodyItems;
+    try {
+        for (let i = 0; i < methods.length; i++) {
+            for (let item in bodyItems) {
+                if (bodyItems[item].customMethod.toLowerCase() === methods[i]) {
+                    router[methods[i]](bodyItems[item].customRoute, (req, res, next) => {
+                        res.locals.response = bodyItems[item].customResponse;
+                        next();
+                    }, handleRequest);
+                }
+            }
         }
-      }
+        next();
+    } catch (error) {
+        res.status(500).send(error).end();
     }
-    next();
-  } catch (error) {
-    res.status(500).send(error).end();
-  }
 }
 
 // Placeholder middleware for making sure dynamic routing works
 const handleRequest = (req, res) => {
-  try {
-    if (req.headers['content-type'].includes('json')
-      || req.headers['content-type'].includes('xml')) {
-      res.status(200).send(res.locals.response);
-      res.end();
-    } else {
-      res.status(400);
-      res.end();
+    try {
+        if (req.headers['content-type'].includes('json') ||
+            req.headers['content-type'].includes('xml')) {
+            res.status(200).send(res.locals.response);
+            res.end();
+        } else {
+            res.status(400);
+            res.end();
+        }
+    } catch {
+        res.status(500);
+        res.end();
     }
-  } catch {
-    res.status(500);
-    res.end();
-  }
 };
 
 // Route for setting up other routes dynamically
 router.use('/676F74E28099656D', setDynamicRoutes, (req, res) => {
-  console.log("ROUTES HAVE BEEN SET");
-  res.status(200).end();
+    console.log("ROUTES HAVE BEEN SET");
+    res.status(200).end();
 });
 
 
 
-
-module.exports = {router, setURLFilePath};
-
+module.exports = { router, setURLFilePath };
